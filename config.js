69a49654c11140a0cf8b344dd122f0ef6c7001ac
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:27:04 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MARTIN GARRIX';
+export let nameSongs = [
+	"BYTE", /*1*/
+	"VIRUS", /*2*/
+	"SUMMER DAYS", /*3*/
+	"FORBIDDEN VOICES", /*4*/
+	"IN THE NAME OF LOVE", /*5*/
+	"SCARED TO BE LONELY", /*6*/
+	"SO FAR AWAY", /*7*/
+	"SUMMER DAYS (REMIX)", /*8*/
+	"USED TO LOVE", /*9*/
+	"MISTAKEN", /*10*/
+	"BURN OUT", /*11*/
+	"OCEAN", /*12*/
+	"HIGH ON LIFE", /*13*/
+	"NO SLEEP", /*14*/
+	"HEAR ME NOW (SLOW+REVERB)", /*15*/
+	"DON'T LOOK DOWN", /*16*/
+	"THE ONLY WAY IS UP", /*17*/
+	"DROWN", /*18*/
+	"PIZZA", /*19*/
+	"FOLLOW", /*20*/
+]
